a5135cd092c09438504c1be945bee108ffa83f65
wzrdk3lly <dkmk1149@gmail.com>
2022-05-29 17:51:57 -0400

    update sketch logic with mouseover eventlistener

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-// create variables for rows and columns
-const rows = 30;
-const columns = rows;
-const totalNumberOfCells = rows * rows;
-
-// select sketch container and set to a var
-let sketchContainer = document.querySelector(".sketch-container");
+//grid to originally start with
+generateGrid();
+acitvateSketch();
 
 
-for(i = 1; i<= (totalNumberOfCells); i++){
-    // create div with class of `sketch-cell` 
-    let sketchCell = document.createElement("div"); 
-    sketchCell.classList.add("sketch-cell");
 
-    //add text and append 
-   
-    sketchCell.style.border = "1px solid black";
-    sketchContainer.appendChild(sketchCell);
+function btnClear(){
+    document.querySelector
 }
-
-// Have an evenly distributed and sized grid that fits into the container
-sketchContainer.style.gridTemplateColumns = `repeat(${rows},1fr)`
-
-
-
 //Make the hover effect by adding event listeners to each of the divs. 
 // when someone hovers over any of the `sketch-pieces` it will change the background color
+function acitvateSketch(color="black"){
+    const cells = document.querySelectorAll(".sketch-cell");
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover",function (){
+        cell.style.backgroundColor = color;
+    });
+});
+}
+
+
+function generateGrid(){
+    const rows = 16;
+    const columns = rows;
+    const totalNumberOfCells = rows * rows;
+
+    // select sketch container and set to a var
+    let sketchContainer = document.querySelector(".sketch-container");
+
+    for(i = 1; i<= (totalNumberOfCells); i++){
+        let sketchCell = document.createElement("div"); 
+        sketchCell.classList.add("sketch-cell");
+        sketchCell.style.border = "1px solid black";
+        sketchContainer.appendChild(sketchCell);
+    }
+
+    // add style for an evenly distributed and sized grid that fits into the container
+    sketchContainer.style.gridTemplateColumns = `repeat(${rows},1fr)`
+}
+
+
+
+
+
+
