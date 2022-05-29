a7d9d33873913cb83182b3fb097a4812eb335c73
wzrdk3lly <dkmk1149@gmail.com>
2022-05-29 13:51:55 -0400

    create project outline with comments explaining future functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create a contianer for where the etch-a-sketch drawing will be 
+
+// create js code that generates a 16X16 grid
+    // break it down into creating a loop that creates a 2x2. 
+    // research how to make the contents auto-size to the container it's in 
+
+//Make the hover effect by adding event listeners to each of the divs. 
+// when someone hovers over any of the `sketch-pieces` it will change the background color
