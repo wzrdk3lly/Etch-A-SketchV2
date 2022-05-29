511595a9c460f322405a9c43380825c1c1cafda3
wzrdk3lly <dkmk1149@gmail.com>
2022-05-29 17:15:49 -0400

    add grid cells via js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-// Create a contianer for where the etch-a-sketch drawing will be 
-console.log("connected")
+// create variables for rows and columns
+const rows = 30;
+const columns = rows;
+const totalNumberOfCells = rows * rows;
 
-// create js code that generates a 16X16 grid
-    // break it down into creating a loop that creates a 2x2. 
-    // research how to make the contents auto-size to the container it's in 
+// select sketch container and set to a var
+let sketchContainer = document.querySelector(".sketch-container");
+
+
+for(i = 1; i<= (totalNumberOfCells); i++){
+    // create div with class of `sketch-cell` 
+    let sketchCell = document.createElement("div"); 
+    sketchCell.classList.add("sketch-cell");
+
+    //add text and append 
+   
+    sketchCell.style.border = "1px solid black";
+    sketchContainer.appendChild(sketchCell);
+}
+
+// Have an evenly distributed and sized grid that fits into the container
+sketchContainer.style.gridTemplateColumns = `repeat(${rows},1fr)`
+
+
 
 //Make the hover effect by adding event listeners to each of the divs. 
 // when someone hovers over any of the `sketch-pieces` it will change the background color
